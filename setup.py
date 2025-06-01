a3d6c464af4f72862175633fd79800b74315c46e
rajeshwari mistri <106986010+RajEshwariCodes@users.noreply.github.com>
2025-06-01 21:44:43 +0530

    Initial commit of pain-imputer library

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ setup(
     description="PAIN : A library for imputing missing values",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/pain-imputer",
+    url="https://github.com/RajEshwariCodes/pain-imputer",
     packages=find_packages(),
     install_requires=[
-        "numpy",
-        "pandas",
-        "scikit-learn",
-        "tensorflow==2.12.0"
+        "numpy==1.21.0",
+        "pandas==1.3.0",
+        "scikit-learn==1.0.0",
+        "tensorflow==2.10.0"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -28,8 +28,5 @@ setup(
     python_requires=">=3.7",
 
     keywords="data-imputation missing-data machine-learning scikit-learn random-forest autoencoder mixed-dataset",
-    project_urls={
-        "Bug Tracker": "https://github.com/parulkumari2707/FRAME-FEATURE-SELECTOR/issues",
-        "Documentation": "https://github.com/parulkumari2707/FRAME-FEATURE-SELECTOR#readme"
     }
 )

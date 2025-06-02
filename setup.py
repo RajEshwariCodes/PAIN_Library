ecc82086bb19828c571c7e565df7a4c4f2ef3f64
rajeshwari mistri <106986010+RajEshwariCodes@users.noreply.github.com>
2025-06-02 11:35:53 +0530

    Initial commit of pain-imputer library

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pain-imputer",
-    version="0.1.0",
+    version="0.1.2",
     author="Rajeshwari Mistri",
     author_email="rajeshwarimistri11@gmail.com",
     description="PAIN : A library for imputing missing values",
